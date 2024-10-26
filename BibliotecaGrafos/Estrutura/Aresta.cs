c8b21b3fe8d60549ad0cceab297c7624f4fbb008
Carol <carolinaabpessoa@gmail.com>
2024-10-25 23:28:02 -0300

    creating main class (Grafo) structure

---
+++ BibliotecaGrafos/Estrutura/Aresta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaGrafos.Estrutura
+{
+    // construindo a classe da aresta que representa as relações entre os vértices
+    public class Aresta(Vertice v_origem, Vertice v_destino, int peso)
+    {
+        public int Id { get; set; }
+        public Vertice VerticeOrigem { get; set; } = v_origem;
+        public Vertice VerticeDestino { get; set; } = v_destino;
+        public int Peso { get; set; } = peso;
+    }
+}
---
+++ BibliotecaGrafos/Estrutura/Grafo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaGrafos.Estrutura
+{
+    // um grafo é um conjunto de vertices e suas relações 
+    public class Grafo(List<Vertice> vertices, List<Aresta> arestas)
+    {
+        // criando lista de vertices (ex: ["A", "B", "C"]) 
+        public List<Vertice> Vertices { get; set; } = vertices;
+
+        // criando lista de arestas (ex: [("A", "B"), ("A", "C"), ("B", "C")]
+        public List<Aresta> Arestas { get; set; } = arestas;
+    }
+}
